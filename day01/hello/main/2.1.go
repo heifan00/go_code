f3c702c2c8b9e2ae060659cf6ce6213e12012f9f
heifan00 <heifan00@dingtalk.com>
2024-01-11 17:50:33 +0800

    update

---
+++ day01/hello/main/2.1.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 11_22_33_44
+	fmt.Println(a)
+}
---
+++ day01/main/if-else.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%2 == 0 || 7%2 == 0 {
+		fmt.Println("either 8 or 7 are even")
+	}
+
+	
+}
